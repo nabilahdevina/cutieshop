f5766fa096cf27475b809752c8165677656a1fc8
“nabilahdevina” <dhevina10@gmail.com>
2024-09-09 21:58:20 +0700

    push models

---
+++ main/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class MoodEntry(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    stock = models.IntegerField()
+    color = models.TextField()
+
+    
